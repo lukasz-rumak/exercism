4cfe8fdcded249c02c80eb94a5301a51812398b3
Lukasz Rumak <lukasz.rumak@newvoicemedia.com>
2020-10-21 16:37:03 +0200

    word count

---
+++ Easy/Word Count/word_count.py
@@ -0,0 +1,29 @@
+import sys
+from collections import OrderedDict
+
+def count_words(input):
+    words = []
+    tmp = ""
+    while input[len(input) - 1] == " ":
+        input = input[:-1]
+    input = input + " "
+    for i in input:
+        if i != " ":
+            tmp = tmp + i
+        else:
+            if tmp != "":
+                words.append(tmp)
+            tmp = ""
+
+    result = {}
+    for w in words:
+        count = 0
+        for i in words:
+            if w == i:
+                count = count + 1
+        result.update({w: count})
+
+    print(result)
+
+if __name__ == "__main__":
+    count_words(str(sys.argv[1]))
