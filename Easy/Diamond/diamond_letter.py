0179e7969273c13ef1f051fd1ceb8d6481387055
Lukasz Rumak <lukasz.rumak@newvoicemedia.com>
2020-10-28 10:18:43 +0100

    diamonds

--- Easy/Diamond/diamond_letter.py
+++ Easy/Diamond/diamond_letter.py
@@ -1,32 +1,34 @@
 import sys
 
 def generate_diamond(letter):
+    if letter.upper() not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        print("Please provide valid letter.")
+        return
     result = []
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    index = alphabet.index(letter.upper())
-    width_and_height = calculate_width_and_height(index)
+    width_and_height = calculate_width_and_height(alphabet.index(letter.upper()))
     spaces = int((width_and_height - 1) / 2)
-    counter = 0
+    index = 0
     switch = False
     for line in range(width_and_height):
         str = ""
         for _ in range(spaces):
             str = str + " "
         if line == 0 or line == width_and_height - 1:
-            str = str + alphabet[counter]
+            str = str + alphabet[index]
         else:
-            str = str + alphabet[counter]
+            str = str + alphabet[index]
             for _ in range(calculate_middle_space(width_and_height, spaces)):
                 str = str + " "
-            str = str + alphabet[counter]
+            str = str + alphabet[index]
         for _ in range(spaces):
             str = str + " "
         if switch is False:
             spaces = spaces - 1
-            counter = counter + 1
+            index = index + 1
         else:
             spaces = spaces + 1
-            counter = counter - 1
+            index = index - 1
         if spaces == 0:
             switch = True
         result.append(str)
