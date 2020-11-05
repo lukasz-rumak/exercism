f152b9791002d00aa18715639341114357c37b73
Lukasz Rumak <lukasz.rumak@newvoicemedia.com>
2020-11-05 15:50:14 +0100

    rail fence cipher

--- Easy/Rail Fence Cipher/rail_fence_cipher_decode.py
+++ Easy/Rail Fence Cipher/rail_fence_cipher_decode.py
@@ -8,51 +8,44 @@ def rail_fence_cipher_decode(sentence, rails):
     sentence_length = len(sentence)
     result = []
     index = 0
+    tmp_length = 0
+    switch_tmp_length = True
 
-    for _ in range(length):
-        result.append(0)
-    for _ in range(length):
-        tmp_length = index
-        while tmp_length < sentence_length:
-            result[index] = result[index] + 1
-            tmp_length = tmp_length + length
-        index = index + 1
-
-    result_merged = []
     for _ in range(rails):
-        result_merged.append(0)
-
-    index = 0
-    for _ in result_merged:
-        if index == 0:
-            result_merged[index] = result[index]
-        elif index == rails - 1:
-            result_merged[index] = result[rails - 1]
-        else:
-            result_merged[index] = result[index] + result[length - index]
-        index = index + 1
-
-    result_str = []
-    for _ in range(rails):
-        result_str.append([])
-    index = 0
-    counter = 0
+        result.append([])
     for s in sentence:
-        if counter < result_merged[index]:
-            result_str[index].append(s)
-            counter = counter + 1
+        if index == 0:
+            if tmp_length < sentence_length:
+                result[index].append(s)
+                tmp_length = tmp_length + length
+            else:
+                index = index + 1
+                result[index].append(s)
+                tmp_length = length - index
+                switch_tmp_length = False
+        elif index < rails - 1:
+            if tmp_length < sentence_length:
+                result[index].append(s)
+                if switch_tmp_length:
+                    tmp_length = tmp_length + length - (index * 2)
+                    switch_tmp_length = False
+                else:
+                    tmp_length = tmp_length + (index * 2)
+                    switch_tmp_length = True
+            else:
+                index = index + 1
+                result[index].append(s)
+                tmp_length = length - index
+                switch_tmp_length = False
         else:
-            index = index + 1
-            counter = 0
-            result_str[index].append(s)
-            counter = counter + 1
+             result[index].append(s)
 
     final = ""
     index = 0
     index_switch = False
-    for _ in range(len(sentence)):
-        final = final + result_str[index][0]
-        result_str[index] = result_str[index][1:]  # delete first element from the list
+    for _ in range(sentence_length):
+        final = final + result[index][0]
+        result[index] = result[index][1:]  # delete first element from the list
         if index_switch:
             index = index - 1
         else:
@@ -73,3 +66,42 @@ def calculate_length(rails):
 if __name__ == "__main__":
     rail_fence_cipher_decode("LKIUAJIMKMEKAAUSSDSRTORZBY", 6)
     # rail_fence_cipher_decode(str(sys.argv[1]), int(sys.argv[2]))
+
+
+    # for _ in range(length):
+    #     result.append(0)
+    # for _ in range(length):
+    #     tmp_length = index
+    #     while tmp_length < sentence_length:
+    #         result[index] = result[index] + 1
+    #         tmp_length = tmp_length + length
+    #     index = index + 1
+    #
+    # result_merged = []
+    # for _ in range(rails):
+    #     result_merged.append(0)
+    #
+    # index = 0
+    # for _ in result_merged:
+    #     if index == 0:
+    #         result_merged[index] = result[index]
+    #     elif index == rails - 1:
+    #         result_merged[index] = result[rails - 1]
+    #     else:
+    #         result_merged[index] = result[index] + result[length - index]
+    #     index = index + 1
+    #
+    # result_str = []
+    # for _ in range(rails):
+    #     result_str.append([])
+    # index = 0
+    # counter = 0
+    # for s in sentence:
+    #     if counter < result_merged[index]:
+    #         result_str[index].append(s)
+    #         counter = counter + 1
+    #     else:
+    #         index = index + 1
+    #         counter = 0
+    #         result_str[index].append(s)
+    #         counter = counter + 1
--- Easy/Rail Fence Cipher/tests_rail_fence_cipher_decode.py
+++ Easy/Rail Fence Cipher/tests_rail_fence_cipher_decode.py
@@ -11,6 +11,46 @@ class RailFenceCipherDecodeTestCase(unittest.TestCase):
         self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LKIUAJIMKMEKAAUSSDSRTORZBY", 6),
                          "LUKASZRUMAKJESTBOSKIIMADRY")
 
+    def test_rail_fence_cipher_decode_happy_path_3(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LKIUAJIMJKMEKAAAUSSDFSRTORIZBY", 6),
+                         "LUKASZRUMAKJESTBOSKIIMADRYIFAJ")
+
+    def test_rail_fence_cipher_decode_happy_path_4(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LKINUAJIMJKMEKAAAUSSDFSRTORIZBY", 6),
+                         "LUKASZRUMAKJESTBOSKIIMADRYIFAJN")
+
+    def test_rail_fence_cipher_decode_happy_path_5(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LKINUAJIMJYKMEKAAAUSSDFSRTORIZBY", 6),
+                         "LUKASZRUMAKJESTBOSKIIMADRYIFAJNY")
+
+    def test_rail_fence_cipher_decode_happy_path_6(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LKSRMKUAZUA", 2),
+                         "LUKASZRUMAK")
+
+    def test_rail_fence_cipher_decode_happy_path_7(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LSMUAZUAKRK", 3),
+                         "LUKASZRUMAK")
+
+    def test_rail_fence_cipher_decode_happy_path_8(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LRUZUKSMKAA", 4),
+                         "LUKASZRUMAK")
+
+    def test_rail_fence_cipher_decode_happy_path_9(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LMUUAKRKAZS", 5),
+                         "LUKASZRUMAK")
+
+    def test_rail_fence_cipher_decode_happy_path_10(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LKUAKMAUSRZ", 6),
+                         "LUKASZRUMAK")
+
+    def test_rail_fence_cipher_decode_happy_path_11(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LUKKAASMZUR", 7),
+                         "LUKASZRUMAK")
+
+    def test_rail_fence_cipher_decode_happy_path_12(self):
+        self.assertEqual(rail_fence_cipher_decode.rail_fence_cipher_decode("LUKASKZARMU", 8),
+                         "LUKASZRUMAK")
+
 
 if __name__ == '__main__':
     unittest.main()
