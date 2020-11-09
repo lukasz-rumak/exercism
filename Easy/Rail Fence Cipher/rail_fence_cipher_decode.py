4e6318abb25e36308eb2407aa777a357f0b76d41
Lukasz Rumak <lukasz.rumak@newvoicemedia.com>
2020-11-09 12:42:30 +0100

    rail fence cipher

--- Easy/Rail Fence Cipher/rail_fence_cipher_decode.py
+++ Easy/Rail Fence Cipher/rail_fence_cipher_decode.py
@@ -8,53 +8,36 @@ def rail_fence_cipher_decode(sentence, rails):
     sentence_length = len(sentence)
     result = []
     index = 0
-    tmp_length = 0
-    switch_tmp_length = True
+    tmp_index = 0
+    switch_tmp_index = True
 
-    for _ in range(rails):
-        result.append([])
+    for _ in sentence:
+        result.append("")
     for s in sentence:
-        if index == 0:
-            if tmp_length < sentence_length:
-                result[index].append(s)
-                tmp_length = tmp_length + length
-            else:
-                index = index + 1
-                result[index].append(s)
-                tmp_length = length - index
-                switch_tmp_length = False
+        if tmp_index >= sentence_length:
+            index = index + 1
+            result[index] = s
+            tmp_index = length - index
+            switch_tmp_index = False
+        elif index == 0:
+            result[tmp_index] = s
+            tmp_index = tmp_index + length
         elif index < rails - 1:
-            if tmp_length < sentence_length:
-                result[index].append(s)
-                if switch_tmp_length:
-                    tmp_length = tmp_length + length - (index * 2)
-                    switch_tmp_length = False
-                else:
-                    tmp_length = tmp_length + (index * 2)
-                    switch_tmp_length = True
+            result[tmp_index] = s
+            if switch_tmp_index:
+                tmp_index = tmp_index + length - (index * 2)
+                switch_tmp_index = False
             else:
-                index = index + 1
-                result[index].append(s)
-                tmp_length = length - index
-                switch_tmp_length = False
+                tmp_index = tmp_index + (index * 2)
+                switch_tmp_index = True
         else:
-             result[index].append(s)
+            tmp_index = tmp_index + length
+            result[tmp_index] = s
+
 
     final = ""
-    index = 0
-    index_switch = False
-    for _ in range(sentence_length):
-        final = final + result[index][0]
-        result[index] = result[index][1:]  # delete first element from the list
-        if index_switch:
-            index = index - 1
-        else:
-            index = index + 1
-        if index == rails:
-            index_switch = True
-            index = index - 2
-        if index == 0:
-            index_switch = False
+    for r in result:
+        final = final + r
 
     return final
 
@@ -66,6 +49,51 @@ def calculate_length(rails):
 if __name__ == "__main__":
     rail_fence_cipher_decode("LKIUAJIMKMEKAAUSSDSRTORZBY", 6)
     # rail_fence_cipher_decode(str(sys.argv[1]), int(sys.argv[2]))
+
+    # for _ in range(rails):
+    #     result.append([])
+    # for s in sentence:
+    #     if index == 0:
+    #         if tmp_length < sentence_length:
+    #             result[index].append(s)
+    #             tmp_length = tmp_length + length
+    #         else:
+    #             index = index + 1
+    #             result[index].append(s)
+    #             tmp_length = length - index
+    #             switch_tmp_length = False
+    #     elif index < rails - 1:
+    #         if tmp_length < sentence_length:
+    #             result[index].append(s)
+    #             if switch_tmp_length:
+    #                 tmp_length = tmp_length + length - (index * 2)
+    #                 switch_tmp_length = False
+    #             else:
+    #                 tmp_length = tmp_length + (index * 2)
+    #                 switch_tmp_length = True
+    #         else:
+    #             index = index + 1
+    #             result[index].append(s)
+    #             tmp_length = length - index
+    #             switch_tmp_length = False
+    #     else:
+    #          result[index].append(s)
+    #
+    # final = ""
+    # index = 0
+    # index_switch = False
+    # for _ in range(sentence_length):
+    #     final = final + result[index][0]
+    #     result[index] = result[index][1:]  # delete first element from the list
+    #     if index_switch:
+    #         index = index - 1
+    #     else:
+    #         index = index + 1
+    #     if index == rails:
+    #         index_switch = True
+    #         index = index - 2
+    #     if index == 0:
+    #         index_switch = False
 
 
     # for _ in range(length):
