d2db074618ff0e72e1bf1c24d35c635f1de85eca
lukasz-rumak <lukasz.rumak.work@gmail.com>
2020-12-01 21:25:23 +0100

    binary search

--- Easy/Binary Search/binary_search.py
+++ Easy/Binary Search/binary_search.py
@@ -12,16 +12,16 @@ def binary_search(array, value):
     for _ in range(len(array) - 1):
         if array[int(mid)] == value:
             return int(mid)
+        if array[int(mid)] > value:
+            end = mid
         else:
-            if array[int(mid)] > value:
-                end = mid
-            else:
-                start = mid
+            start = mid
         mid = int(start + ((end - start) / 2))
         if mid == start or mid == end:
             return None
+    return None
 
 
 if __name__ == "__main__":
-    binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 1)
-    #quicksort(str(sys.argv[1]))
+    binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], 19)
+    #binary_search(str(sys.argv[1]))
--- Easy/Binary Search/test_binary_search.py
+++ Easy/Binary Search/test_binary_search.py
@@ -3,25 +3,25 @@ import binary_search
 
 
 class BinarySearchTestCase(unittest.TestCase):
-    def test_binary_search_happy_path_1(self):
+    def test_binary_search_happy_path_01(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 2), 1)
 
-    def test_binary_search_happy_path_2(self):
+    def test_binary_search_happy_path_02(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 19), 18)
 
-    def test_binary_search_happy_path_3(self):
+    def test_binary_search_happy_path_03(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 10), 9)
 
-    def test_binary_search_happy_path_4(self):
+    def test_binary_search_happy_path_04(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 9), 8)
 
-    def test_binary_search_happy_path_5(self):
+    def test_binary_search_happy_path_05(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 11), 10)
 
-    def test_binary_search_happy_path_6(self):
+    def test_binary_search_happy_path_06(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 1), 0)
 
-    def test_binary_search_happy_path_9(self):
+    def test_binary_search_happy_path_09(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 20), 19)
 
     def test_binary_search_happy_path_10(self):
@@ -33,6 +33,8 @@ class BinarySearchTestCase(unittest.TestCase):
     def test_binary_search_happy_path_12(self):
         self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20], 19), None)
 
+    def test_binary_search_happy_path_13(self):
+        self.assertEqual(binary_search.binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], 19), 18)
 
 if __name__ == '__main__':
     unittest.main()
